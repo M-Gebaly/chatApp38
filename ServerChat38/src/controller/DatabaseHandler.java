01b1c31415b4d755c40bceef6688b1baa71edadd
M-Gebaly <M-Gebaly@users.noreply.github.com>
2018-02-07 15:44:23 +0200

    Interface DatabaseHandler and its implementation class

---
+++ ServerChat38/src/controller/DatabaseHandler.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+import common.User;
+import java.sql.SQLException;
+import java.util.ArrayList;
+/**
+ *
+ * @author M.Gebaly
+ */
+public interface DatabaseHandler {
+    boolean insertUser(User user) throws SQLException;
+    User loginHandler(String email, String password) throws SQLException;
+    ArrayList<User> friendListHandler(int id) throws SQLException;
+    
+}
---
+++ ServerChat38/src/controller/DatabaseHandlerImp.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import common.User;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author M.Gebaly
+ */
+public class DatabaseHandlerImp implements DatabaseHandler{
+
+    @Override
+    public boolean insertUser(User user) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User loginHandler(String email, String password) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<User> friendListHandler(int id) throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
