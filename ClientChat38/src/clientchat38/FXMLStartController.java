b9b151d4eab84a5b3db9828f5e0a3337b0b5bf0d
M-Gebaly <M-Gebaly@users.noreply.github.com>
2018-02-07 08:17:33 +0200

    register and login design

--- ClientChat38/src/clientchat38/FXMLStartController.java
+++ ClientChat38/src/clientchat38/FXMLStartController.java
@@ -5,21 +5,26 @@
  */
 package clientchat38;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
  * @author M.Gebaly
  */
 public class FXMLStartController implements Initializable {
-    
-    
-    
+
 //    @FXML
 //    private Label label;
 //    
@@ -28,10 +33,35 @@ public class FXMLStartController implements Initializable {
 //        System.out.println("You clicked me!");
 //        label.setText("Hello World!");
 //    }
+    @FXML
+    private Hyperlink goToRegist;
     
+    @FXML
+    private AnchorPane loginPane;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    @FXML
+    private void registerAction(ActionEvent event){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLRegistration.fxml"));
+            Parent root = loader.load();
+            FXMLRegistrationController controller = loader.getController();
+            //controller.setText(nameTxtField.getText());
+            
+            Scene scene = new Scene(root);
+            
+            Stage stage = (Stage) loginPane.getScene().getWindow();
+            stage.setTitle("Register Page");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
     
+
 }
