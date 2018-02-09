48b29d1d05ebc4330072c2813809db5f705e7cdc
M-Gebaly <M-Gebaly@users.noreply.github.com>
2018-02-09 11:30:53 +0200

    lagin and regitration and empty constractor for user

--- ClientChat38/src/clientchat38/FXMLRegistrationController.java
+++ ClientChat38/src/clientchat38/FXMLRegistrationController.java
@@ -6,9 +6,23 @@
 package clientchat38;
 
 
+import common.ServerInterface;
+import common.User;
 import java.net.URL;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 
 /**
@@ -21,15 +35,105 @@ public class FXMLRegistrationController implements Initializable {
     /**
      * Initializes the controller class.
      */
-
+    @FXML
+    public TextField rname;
+    
+    @FXML
+    public TextField rpassword;
+    
+    @FXML
+    public TextField remail;
+    
+    @FXML
+    public RadioButton male;
+    
+    @FXML
+    public RadioButton female;
+    
+    @FXML
+    public Button signup_btn;
+    
+    ServerInterface server;
 
     public FXMLRegistrationController() {
-
+        try {
+            Registry registry = LocateRegistry.getRegistry(2000);
+            server = (ServerInterface) registry.lookup("chat");
+        } catch (RemoteException ex) {
+            Logger.getLogger(FXMLRegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NotBoundException ex) {
+            Logger.getLogger(FXMLRegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
      
-    }    
+    }
+
+    @FXML    
+    private void signUpAction(ActionEvent event){
+        String name = rname.getText();
+        String Password = rpassword.getText();
+        String email = remail.getText();
+        
+        boolean isMale = male.isSelected();
+        boolean isFemale = female.isSelected();
+        
+        if(name.equals("") || Password.equals("") || email.equals("")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning Dialog");
+            alert.setHeaderText("Look, a Warning Dialog");
+            
+            if (name.equals("") ){
+                alert.setContentText("You must enter your name");
+                alert.showAndWait();
+            }
+            if (Password.equals("") ){
+                alert.setContentText("You must enter your password");
+                alert.showAndWait();
+            }
+            if (email.equals("") ){
+                alert.setContentText("You must enter your email");
+                alert.showAndWait();
+            }
+            
+        }else if (!isFemale && !isMale) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning Dialog");
+            alert.setHeaderText("Look, a Warning Dialog");
+            alert.setContentText("You must enter your Ginder");
+            alert.showAndWait();
+        }else{
+            User user  = new User();
+            user.setName(name);
+            user.setPassword(Password);
+            user.setEmail(email);
+            if(isMale)
+                user.setGender("male");
+            if(isFemale)
+                user.setGender("female");
+            boolean result;
+            try {
+                result = server.addNewUser(user);
+                if (!result) {
+                    Alert alert = new Alert(Alert.AlertType.WARNING);
+                    alert.setTitle("Warning Dialog");
+                    alert.setHeaderText("Look, a Warning Dialog");
+                    alert.setContentText("Falied");
+                    alert.showAndWait();
+                }else{
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Dialog");
+                    alert.setHeaderText("Look, an Information Dialog");
+                    alert.setContentText("Regist Success");
+                    alert.showAndWait();
+                }
+            } catch (RemoteException ex) {
+                Logger.getLogger(FXMLRegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+    }
     
 }
--- ClientChat38/src/clientchat38/FXMLStartController.java
+++ ClientChat38/src/clientchat38/FXMLStartController.java
@@ -6,6 +6,7 @@
 package clientchat38;
 
 import common.ServerInterface;
+import common.User;
 import java.io.IOException;
 import java.net.URL;
 import java.rmi.NotBoundException;
@@ -22,6 +23,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
@@ -46,7 +49,7 @@ public class FXMLStartController implements Initializable {
 //    }
     @FXML
     private Hyperlink goToRegist;
-    
+
     @FXML
     private AnchorPane loginPane;
     ServerInterface server = null;
@@ -55,10 +58,10 @@ public class FXMLStartController implements Initializable {
     @FXML
     public TextField password;
     @FXML
-    public Button signUpBtn;
+    public Button loginpBtn;
 
     public FXMLStartController() {
-          try {
+        try {
             Registry registry = LocateRegistry.getRegistry(2000);
             server = (ServerInterface) registry.lookup("chat");
         } catch (RemoteException ex) {
@@ -67,32 +70,71 @@ public class FXMLStartController implements Initializable {
             Logger.getLogger(FXMLRegistrationController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-          signUpBtn.setOnMouseClicked(new  EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                try {
-                    server.login(email.getText(), password.getText());
-                } catch (RemoteException ex) {
-                    Logger.getLogger(FXMLRegistrationController.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });         
+//          signUpBtn.setOnMouseClicked(new  EventHandler<MouseEvent>() {
+//            @Override
+//            public void handle(MouseEvent event) {
+//                try {
+//                    server.login(email.getText(), password.getText());
+//                } catch (RemoteException ex) {
+//                    Logger.getLogger(FXMLRegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+//                }
+//            }
+//        });         
     }
 
     @FXML
-    private void registerAction(ActionEvent event){
+    private void loginAction(ActionEvent event) {
+        String emailv = email.getText();
+        String passwordv = password.getText();
+        if (!emailv.equals("") && !passwordv.equals("")) {
+            try {
+                User user;
+                user = server.login(email.getText(), password.getText());
+                if(user != null){
+                    Alert alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Dialog");
+                    alert.setHeaderText("Look, an Information Dialog" + user.getEmail() + "  " + user.getPassword());
+                    alert.setContentText("Login Success");
+
+                    alert.showAndWait();
+                }else{
+                    Alert alert = new Alert(AlertType.INFORMATION);
+                    alert.setTitle("Information Dialog");
+                    alert.setHeaderText("Look, an Information Dialog");
+                    alert.setContentText("Incorrect password or email");
+                    alert.showAndWait();
+                }
+            } catch (RemoteException ex) {
+                Logger.getLogger(FXMLRegistrationController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            Alert alert = new Alert(AlertType.WARNING);
+            alert.setTitle("Warning Dialog");
+            alert.setHeaderText("Look, a Warning Dialog");
+            if (email.equals("")) {
+                alert.setContentText("You must enter your email");
+            }
+            if (password.equals("")) {
+                alert.setContentText("You must enter your password");
+            }
+
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void registerAction(ActionEvent event) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLRegistration.fxml"));
             Parent root = loader.load();
             FXMLRegistrationController controller = loader.getController();
             //controller.setText(nameTxtField.getText());
-            
+
             Scene scene = new Scene(root);
-            
+
             Stage stage = (Stage) loginPane.getScene().getWindow();
             stage.setTitle("Register Page");
             stage.setScene(scene);
@@ -101,6 +143,5 @@ public class FXMLStartController implements Initializable {
             ex.printStackTrace();
         }
     }
-    
 
 }
